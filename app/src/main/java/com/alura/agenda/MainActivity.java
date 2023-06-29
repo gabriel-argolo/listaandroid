4b9f2c04de5c48570bd42c38ad657e7901c60903
Gabriel Argolo <gabriel_argolo@outlook.com.br>
2023-06-29 15:57:41 -0300

    primeiro dia

---
+++ app/src/main/java/com/alura/agenda/MainActivity.java
@@ -0,0 +1,24 @@
+package com.alura.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+
+       List<String> alunos = new ArrayList<>(Arrays.asList("Gabriel", "Pedro", "Jose", "Carlos"));
+
+       ListView lista = findViewById(R.id.lista_de_alunos);
+       lista.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+    }
+}
